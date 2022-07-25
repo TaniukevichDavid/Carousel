42142c57b66f974c1c3b6b196dea47a578042b33
David Taniukevich <tanuk2001@mail.ru>
2022-07-25 14:30:17 +0300

    Initial Commit

---
+++ Carousel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Carousel
+//
+//  Created by David Taniukevich on 25.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
